91e892a20582163787373c91da07368666a74b2b
Dylan <safirodylan@gmail.com>
2025-02-02 01:12:21 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -7,5 +7,12 @@ console.log("Eligible for 15% off discount")
     console.log("Not eligible for discount")
 }
 let finalAmount = purchaseAmount // Declares final amount variable
-console.log(`Final amount: $${finalAmount}`) // Outputs final amount
+console.log(`Final Amount: $${finalAmount}`) // Outputs final amount
 
+// Task 2: Sales Report
+let sales = [85, 90, 67, 156, 25] // Declares array of sales figures
+let totalSales = 0 // Declares total sales variable
+for (let sale = 0; sale < sales.length; sale++){ // Totals sales until all sales figures are added
+totalSales += sales[sale] // Sums sales figure amount into totalSales
+}
+console.log(`Total Sales: $${totalSales}`) // Outputs total sales
